41d193d8a01feba469b200b29b9b4480d47b65be
Jakub Sitnicki <jkbs@redhat.com>
2018-03-27 13:53:26 +0200

    WIP: Datapath: Add support for creating multiple routers from context

--- rally_ovs/plugins/ovs/context/datapath.py
+++ rally_ovs/plugins/ovs/context/datapath.py
@@ -16,9 +16,7 @@ LOG = logging.getLogger(__name__)
 
 @context.configure(name="datapath", order=115)
 class Datapath(context.Context):
-    """
-    Create datapath resources (logical switches or logical routers).
-    """
+    """Create datapath resources (logical switches or logical routers)."""
 
     CONFIG_SCHEMA = {
         "type": "object",
@@ -29,20 +27,23 @@ class Datapath(context.Context):
                 "properties": {
                     "amount": {
                         "type": "integer",
-                        "minimum": 0
+                        "minimum": 0,
                     },
                     "batch": {
                         "type": "integer",
-                        "minimum": 1
+                        "minimum": 1,
                     }
                 },
-                "additionalProperties": False
-            }
+                "additionalProperties": False,
+            },
+            "cleanup": {"type": "boolean"},
         },
-        "additionalProperties": False
+        "additionalProperties": False,
     }
 
     DEFAULT_CONFIG = {
+        "router_create_args": {"amount": 0},
+        "cleanup": True,
     }
 
     RESOURCE_NAME_FORMAT = "lrouter_XXXXXX_XXXXXX"
@@ -50,31 +51,44 @@ class Datapath(context.Context):
     def setup(self):
         self._init_clients()
 
-        ovn_nbctl = self.controller_client("ovn-nbctl")
-        ovn_nbctl.set_sandbox("controller-sandbox", self.install_method)
+        router_create_args = self.config["router_create_args"]
+        routers = self._add_routers(router_create_args)
+        self.context["datapaths"] = {
+            "routers": routers,
+        }
 
-        lrouters = []
-
-        name = self.generate_random_name()
-        lrouter = ovn_nbctl.lrouter_add(name)
-        lrouters.append(lrouter)
-
-        self.context["datapaths"] = { "lrouters": lrouters }
+    def _add_routers(self, router_create_args):
+        routers = []
+        ovn_nbctl = self._get_ovn_nbctl()
+        for i in range(router_create_args["amount"]):
+            name = self.generate_random_name()
+            lr = ovn_nbctl.lrouter_add(name)
+            routers.append(lr)
+        return routers
 
     def cleanup(self):
-        pass
+        if not self.config["cleanup"]:
+            return
 
     def _init_clients(self):
-        # FIXME: This code is duplicated in OvnNorthbound.setup() and
-        # OvsScenario.__init__(). Create a helper?
-        multihost_info = self.context["ovn_multihost"]
+        # FIXME: Already exists in:
+        # - OvnNorthbound.setup()
+        # - OvsScenario.__init__()
 
+        multihost_info = self.context["ovn_multihost"]
         for k, v in six.iteritems(multihost_info["controller"]):
             cred = v["credential"]
             self._controller_clients = ovsclients.Clients(cred)
 
-        self.install_method = multihost_info["install_method"]
+        self._install_method = multihost_info["install_method"]
 
-    def controller_client(self, client_type="ssh"):
+    def _get_controller_client(self, client_type="ssh"):
+        # Returns a remote client for the controller node
         client = getattr(self._controller_clients, client_type)
         return client()
+
+    def _get_ovn_nbctl(self):
+        # Returns ovn-nbctl client for the controller sandbox
+        ovn_nbctl = self._get_controller_client("ovn-nbctl")
+        ovn_nbctl.set_sandbox("controller-sandbox", self._install_method)
+        return ovn_nbctl
--- rally_ovs/tests/unit/plugins/ovs/context/test_datapath.py
+++ rally_ovs/tests/unit/plugins/ovs/context/test_datapath.py
@@ -1,63 +1,97 @@
+# Copyright 2018 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
 import ddt
 import mock
 
-from rally_ovs.plugins.ovs.context import datapath
+from jsonschema.exceptions import ValidationError
+from rally_ovs.plugins.ovs.context.datapath import Datapath
 from tests.unit import test
-
-
-# case 1: just lrouter
-# cast 2: just lswitch
-# case 3: lrouter and lswitch
-# case 4: lrouter and lswitch linked
 
 @ddt.ddt
 class DatapathTestCase(test.TestCase):
 
-    def test__init__(self):
-        context = datapath.Datapath({})
+    @ddt.data({},
+              {"amount": 0},
+              {"amount": 1},
+              {"amount": 2},
+              {"amount": 0, "batch": 1},
+              {"amount": 1, "batch": 1},
+              {"amount": 2, "batch": 1},
+              {"amount": 2, "batch": 2})
+    def test_valid_router_config(self, router_create_args):
+        config = {"router_create_args": router_create_args}
+        Datapath.validate(config)
 
-    def test_validate(self):
-        config = {
-            "router_create_args": {
-                "amount": 0,
-            },
-        }
-        datapath.Datapath.validate(config)
+    @ddt.data({"amount": -1},
+              {"amount": 0, "batch": 0},
+              {"amount": 1, "batch": -1})
+    def test_invalid_router_config(self, router_create_args):
+        config = {"router_create_args": router_create_args}
+        with self.assertRaisesRegexp(ValidationError, ""): # Ignore message
+              Datapath.validate(config)
 
-    @mock.patch("rally_ovs.plugins.ovs.ovsclients_impl.OvnNbctl.create_client")
-    def test_setup(self, mock_create_client):
-        ctx = {
+    def get_fake_context(self, **datapath_config):
+        return {
             "task": {
-                "uuid": "fake-task-uuid",
+                "uuid": "fake_task_uuid",
             },
             "ovn_multihost": {
                 "controller": {
                     "fake-controller-node": {
                         "name": "fake-controller-node",
                         "credential": {
-                            "user": "fake-user",
-                            "host": "fake-host",
+                            "user": "fake_user",
+                            "host": "fake_host",
                             "port": -1,
-                            "key": "fake-key",
-                            "password": "fake-password",
+                            "key": "fake_key",
+                            "password": "fake_password",
                         },
                     },
                 },
-                "install_method": "fake-install-method",
+                "install_method": "fake_install_method",
             },
-
+            "config": {
+                "datapath": datapath_config,
+            },
         }
 
+    def test_init_default(self):
+        Datapath({})
+
+    @ddt.data({"router_create_args": {"amount": 1},
+               "routers": [{"name": "lrouter_1"}]},
+              {"router_create_args": {"amount": 2},
+               "routers": [{"name", "lrouter_1"},
+                           {"name": "lrouter_2"}]},
+              {"router_create_args": {"amount": 3},
+               "routers": [{"name": "lrouter_1"},
+                           {"name": "lrouter_2"},
+                           {"name": "lrouter_3"}]})
+    @ddt.unpack
+    @mock.patch("rally_ovs.plugins.ovs.ovsclients_impl.OvnNbctl.create_client")
+    def test_add_only_routers(self, mock_create_client,
+                              router_create_args, routers):
         mock_ovnbctl = mock.MagicMock()
-        mock_ovnbctl.lrouter_add.return_value = {"name": "lrouter_TEST"}
+        mock_ovnbctl.lrouter_add.side_effect = routers
         mock_create_client.return_value = mock_ovnbctl
 
-        dp_context = datapath.Datapath(ctx)
-
+        context = self.get_fake_context(router_create_args=router_create_args)
+        dp_context = Datapath(context)
         dp_context.setup()
 
-        expected_lrouters = [{"name": "lrouter_TEST"}]
-        actual_lrouters = dp_context.context["datapaths"]["lrouters"]
-
-        self.assertSequenceEqual(sorted(expected_lrouters),
-                                 sorted(actual_lrouters))
+        expected_routers = routers
+        actual_routers = dp_context.context["datapaths"]["routers"]
+        self.assertSequenceEqual(sorted(expected_routers),
+                                 sorted(actual_routers))
